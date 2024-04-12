a6be5fd19a24636cec519a4dee7fdf3b7c0a9213
Anastasiia Prokhorova <naumtasya@gmail.com>
2024-04-13 00:41:35 +0700

    Add Task2

--- cypress.config.mobile.js
+++ cypress.config.mobile.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
+  env: {
+    viewportWidth: 360,
+    viewportHeight: 640,
+  },
   e2e: {
-    baseUrl:'http://localhost:3000/',
-    retries: 0,
+    baseUrl: "http://localhost:3000",    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: 0
   },
 });
--- cypress.config_2.js
+++ cypress.config_2.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
+  env: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+  },
   e2e: {
-    baseUrl:'http://localhost:3000/',
-    retries: 0,
+    baseUrl: "http://localhost:3000",    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: 0
   },
 });
